1e3706f34f3cca3d677b3e25eeb285bf3280384f
Sherban Software <sherbansoftware@gmail.com>
2018-09-10 08:29:44 +0300

    Add beans

---
+++ sampleApp/src/main/java/com/sherbansoftware/sample/SampleAppApplication.java
@@ -0,0 +1,21 @@
+package com.sherbansoftware.sample;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.sherbansoftware.logic.ICustomerService;
+
+@SpringBootApplication
+public class SampleAppApplication {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(SampleAppApplication.class, args);
+		// ICustomerService iCustomerService = new CustomerServiceImpl();
+
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ICustomerService iCustomerService = applicationContext.getBean("customerService", ICustomerService.class);
+		String firstName = iCustomerService.findAll().get(0).getFirstName();
+		System.out.println("First name is: " + firstName);
+	}
+}
